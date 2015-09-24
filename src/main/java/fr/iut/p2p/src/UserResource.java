d6288e27f5a284ea129973d97657c9e4821a0df3
Antoine DUQUENNOY <antoine.duquennoy@etudiant.univ-lille1.fr>
2015-09-24 15:06:20 +0200

    quelque classe +test

---
+++ src/main/java/fr/iut/p2p/src/UserResource.java
@@ -0,0 +1,16 @@
+package fr.iut.p2p.src;
+
+import javax.ws.rs.Path;
+
+/**
+ * Ressource User (accessible avec le chemin "/users")
+ */
+@Path("users")
+public class UserResource {
+
+	/**
+	 * Une ressource doit avoir un contructeur (éventuellement sans arguments)
+	 */
+	public UserResource() {
+	}
+}
